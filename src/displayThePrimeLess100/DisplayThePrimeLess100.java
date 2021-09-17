5fd69abcd2ceade923dd223e714d8c0161403f49
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-17 12:29:09 +0700

    upgrade find Primes less number

--- src/displayThePrimeLess100/DisplayThePrimeLess100.java
+++ src/displayThePrimeLess100/DisplayThePrimeLess100.java
@@ -9,27 +9,24 @@ public class DisplayThePrimeLess100 {
         Scanner inputMax = new Scanner(System.in);
         System.out.println("Enter the number whose primes are less than this number:");
         int max = inputMax.nextInt();
-        display(max);
+        int N = 2;
+        while (N < max) {
+            if (!checkPrime(N)) {
+                System.out.printf("%d, ", N);
+            }
+            N++;
+        }
 
     }
-    public static void display(int max) {
-        int N = 2;
-
-        while (N < max) {
-            boolean flag = false;
-            for (int i = 2; i <= Math.sqrt(N); i++) {
-                if (N % i == 0) {
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag) {
-                System.out.printf("%d, ", N);
-                N++;
-            } else {
-                N++;
+    public static boolean checkPrime(int N) {
+        boolean flag = false;
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (N % i == 0) {
+                flag = true;
+                break;
             }
         }
-    }
+        return flag;
 
+    }
 }
