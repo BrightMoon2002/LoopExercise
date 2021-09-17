cc004584cb70ed44315506e87dbf24e36abac2dd
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-17 11:38:12 +0700

    update find 20 Primes

--- src/display20Prime/Display20Prime.java
+++ src/display20Prime/Display20Prime.java
@@ -9,30 +9,27 @@ public class Display20Prime {
         Scanner inputNumber = new Scanner(System.in);
         System.out.println("Enter number of Prime you want to display:");
         int number = inputNumber.nextInt();
-        Prime(number);
-
-    }
-
-    public static void Prime(int number) {
         int count = 0;
         int N = 2;
-
         while (count < number) {
-            boolean flag = false;
-            for (int i = 2; i <= Math.sqrt(N); i++) {
-                if (N % i == 0) {
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag) {
+            if (Prime(N)) {
                 count++;
-                System.out.print(N + ", ");
-                N++;
-            } else {
-                N++;
+                System.out.printf("%d, ", N);
             }
+            N++;
         }
     }
 
-}
+    public static boolean Prime(int number) {
+       boolean flag = true;
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            return flag;
+        }
+    }
+
+
